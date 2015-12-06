acdd521cca8c2c8c02721537c1a803228038e49d
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-12-06 02:26:27 -0500

    Create controller

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,5 +1,19 @@
 class AppController < ApplicationController
+  before_action :require_login, only: [:savethedate, :rsvp, :registry]
+
   def login
+    today = DateTime.now
+    wedding_day = Date.parse "2016-07-30"
+    @date_diff = (wedding_day - today).to_i
+  end
+
+  def login_attempt
+    if params[:pword_attempt] == 'doughnuts'
+      cookies[:logged_in] = { value: true, expires: 1.day.from_now.utc}
+      redirect_to savethedate_path
+    else
+
+    end
   end
 
   def savethedate
@@ -9,5 +23,11 @@ class AppController < ApplicationController
   end
 
   def registry
+  end
+
+  private
+
+  def require_login
+    redirect_to login_path unless cookies[:logged_in]
   end
 end

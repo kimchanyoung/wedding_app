aef9071d85ad29968ff421a1e6eae6ac5ff4a077
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-12-06 02:27:12 -0500

    Add commented out code for shaking pword field

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// $(document).ready(function(){
+//   $('#login-form').submit(function(e){
+//     e.preventDefault();
+//     if ($('#pword-attempt')[0].value === password) {
+//       createCookie("loggedIn", "true", 1);
+//       window.location = '../savethedate'
+//     }
+//     else {
+//       $('#pword-attempt').effect('shake', {distance: 10});
+//       $('#pword-attempt')[0].value='';
+//     }
+//   })
+// });
